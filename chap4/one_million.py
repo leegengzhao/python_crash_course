d33caec397d833840f71dfdcd9c785ed7b8a7e8b
leegengzhao <leegzhao@yahoo.com>
2017-04-12 20:40:28 -0700

    Added lines for learning slicing list and tuple

--- chap4/one_million.py
+++ chap4/one_million.py
@@ -1,24 +1,42 @@
 # List of numbers from 1 to 1 million
-numbers = list(range(1,1000001))
-#print(numbers)
-print(min(numbers))
-print(max(numbers))
-print(sum(numbers))
+million_numbers = list(range(1,1000001))
+#print(million_numbers)
+print(min(million_numbers))
+print(max(million_numbers))
+print(sum(million_numbers))
 # Odd numbers from 1 to 20
-numbers = list(range(1,20,2))
-print(numbers)
+odd_numbers = list(range(1,20,2))
+print(odd_numbers)
 # Multiple of 3 from 3 to 30
-numbers = []
+multi_3_numbers = []
 for number in range(3,31,3):
-	numbers.append(number)
-print(numbers)
+	multi_3_numbers.append(number)
+print(multi_3_numbers)
 # First 10 cubes
-numbers = []
+cubes = []
 for number in range(1,11):
-	numbers.append(number**3)
-for number in numbers:
+	cubes.append(number**3)
+for number in cubes:
 	print(number)
 # List comprehension
-numbers = [value**3 for value in range(1,11)]
-print(numbers)
-print(numbers)
+cubes = []
+cubes = [value**3 for value in range(1,11)]
+print(cubes)
+# Copy list with slice
+cubes_copy = cubes[:]
+print(cubes_copy)
+# First 3 and last 3
+print(cubes_copy[0:3])
+print(cubes[-3:])
+# Make tuple from list
+li=[1,2,'here is my name','lee zhao']
+tup = tuple(li)
+print(tup)
+
+
+
+
+
+
+
+
